f8f6d459f53ee55ba8b0cb5117be4b41f517935d
Kiran <145902144+KiranGadhavi@users.noreply.github.com>
2023-11-20 16:13:35 +0000

    add all new changes

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import AiHeader from './Components/AiHeader'
+import AiFooter from './Components/AiFooter'
+
+import AiDataContainer from './Components/AiDataContainer'
+
+function App() {
+  
+  return (
+    <> 
+   <AiHeader></AiHeader>
+  
+   <AiDataContainer></AiDataContainer>
+  
+      
+      <AiFooter></AiFooter>
+    </>
+  )
+}
+
+export default App
